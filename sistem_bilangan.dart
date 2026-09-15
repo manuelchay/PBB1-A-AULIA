dc60be8ea5d5ff2bc6e0cc6474a41b15572e31ba
nosungi. <marmutketibandinoo@gmail.com>
2026-09-15 11:22:08 +0700

    Initial project

---
+++ sistem_bilangan.dart
@@ -0,0 +1,11 @@
+void main() {
+int decimal = 10;
+int octal = int.parse('12', radix: 8);
+int binary = int.parse('1010', radix: 2);
+int hexadecimal = 0xA;
+print("Aulia suka JKT48, Hearts2Hearts, aespa, KiiiKii, Baby Monster");
+print(decimal);
+print(binary);
+print(hexadecimal);
+print(octal);
+}
